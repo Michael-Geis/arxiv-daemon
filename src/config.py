4213849977d69fd8f21d980cd647b641f985aeb5
Michael-Geis <leamy1@gmail.com>
2023-10-05 14:55:15 -0400

    changed config dirs to be subdirs of mounted user-dir

--- src/config.py
+++ src/config.py
@@ -1,8 +1,8 @@
 ## Choose to use static or dynamic source/target directories:
-SET_DIRS_AT_RUNTIME = True
+SET_DIRS_AT_RUNTIME = False
 
 ## Absolute path of the directory your downloads are automatically stored in
-SOURCE_DIR = "path/to/downloads/directory"
+SOURCE_DIR = "user-dir/downloads"
 
 ## Absolute path of the directory you'd like to store your papers in
-TARGET_DIR = "path/to/paper/directory"
+TARGET_DIR = "user-dir/desktop"
